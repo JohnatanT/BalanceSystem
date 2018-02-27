61f7bd07493e083e53b3d46a99f7a9778845be2d
Johnatan Tavares <johnatan12014@hotmail.com>
2018-02-27 14:13:33 -0300

    Seed de Users criado

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       DB::table('users')->insert([
+            'name' => 'Johnatan Tavares',
+            'email' => 'johnatan12014@hotmail.com',
+            'password' => bcrypt('123456'),
+       ]);
+    }
+}
