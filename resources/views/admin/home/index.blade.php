1adf2d57d6c495f3672c09993ba22bb67c1c194a
Johnatan Tavares <johnatan12014@hotmail.com>
2018-02-27 15:14:14 -0300

    Rotas e Views do Site e Painel criadas

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -1,9 +1,9 @@
 @extends('adminlte::page')
 
-@section('title', 'AdminLTE')
+@section('title', 'Home Dashboard')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1>Painel de Controle</h1>
 @stop
 
 @section('content')
