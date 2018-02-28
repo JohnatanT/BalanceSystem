6f018b665d489ae2c97d5719c12f21066168ecd2
Johnatan Tavares <johnatan12014@hotmail.com>
2018-02-28 15:22:21 -0300

    Layout Exibição de Saldo

---
+++ resources/views/admin/balance/index.blade.php
@@ -0,0 +1,33 @@
+@extends('adminlte::page')
+
+@section('title', 'Saldo')
+
+@section('content_header')
+    <h1>Saldo</h1>
+
+    <ol class="breadcrumb"> 
+        <li><a href="">Home</a></li>
+        <li><a href="">Saldo</a></li>
+    </ol>
+@stop
+
+@section('content')
+    <div class="box">
+        <div class="box-header">
+            <a href="" class="btn btn-primary"><i class="fa fa-credit-card"></i> Recarregar</a>
+            <a href="" class="btn btn-danger"><i class="fa fa-shopping-cart"></i> Sacar</a>
+        </div>
+        <div class="box-body">
+            <div class="small-box bg-green">
+                <div class="inner">
+                    <h3>R$ 150</h3>
+                    <p>Saldo</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-bag"></i>
+                </div>
+                <a href="#" class="small-box-footer">Histórico <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+    </div>
+@stop
